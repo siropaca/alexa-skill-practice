d07994a4fafd341d79a8190750a667c5444e44da
shirai ryohei <shirai.ryohei0302@gmail.com>
2020-02-19 20:04:43 +0900

    modified: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ZIP_FILENAME="my-function"
-LAMDBA_FUNCNAME="alexaPractice"
+ZIP_FILENAME="function"
+LAMBDA_FUNCNAME="alexaPractice"
 
 echo "Compressing..."
 zip -rq $ZIP_FILENAME.zip * -x deploy.sh
 
 echo "Uploading..."
 aws lambda update-function-code  \
-   --function-name $LAMDBA_FUNCNAME  \
+   --function-name $LAMBDA_FUNCNAME  \
    --zip-file fileb://$ZIP_FILENAME.zip
 
 echo "Cleaning..."
